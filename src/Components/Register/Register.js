b7596c7004c5b5af18c4f73b57cdcd5ba108cb57
Turjo-abhishek <turjoabhishek@gmail.com>
2022-10-26 15:15:33 +0600

    added register page and added toggle to login and register page

--- src/Components/Register/Register.js
+++ src/Components/Register/Register.js
@@ -1,1 +1,54 @@
+import React from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import { Link } from "react-router-dom";
 
+const Register = () => {
+  return (
+    <>
+      <Form className="w-25 mx-auto border rounded mt-5 p-3">
+        <h2 className="mb-3">Register</h2>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Your Name</Form.Label>
+          <Form.Control name="name" type="text" placeholder="Enter your name" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Photo Url</Form.Label>
+          <Form.Control
+            name="photoUrl"
+            type="text"
+            placeholder="Enter the url"
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control name="email" type="email" placeholder="Enter email" />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            name="password"
+            type="password"
+            placeholder="Password"
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="Check me out" />
+        </Form.Group>
+        <Button className="w-100" variant="primary" type="submit">
+          Register
+        </Button>
+      </Form>
+      <p className="text-center mt-2">
+        Already Have an Acount?
+        <Link to="/login" className="text-decoration-none">
+          {" "}
+          Login here!
+        </Link>
+      </p>
+    </>
+  );
+};
+
+export default Register;
