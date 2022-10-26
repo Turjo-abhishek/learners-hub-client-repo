1d067cd76435121ca61ff1e3f8dab27900e50efb
Turjo-abhishek <turjoabhishek@gmail.com>
2022-10-26 13:07:57 +0600

    started the client side project and added all the necessary packages including bootstrap, react-router-dom and firebase

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDRDmHda5882QkU4EMC8RdbL5QN6Aa6igk",
+  authDomain: "learners-hub-client-30963.firebaseapp.com",
+  projectId: "learners-hub-client-30963",
+  storageBucket: "learners-hub-client-30963.appspot.com",
+  messagingSenderId: "569203974449",
+  appId: "1:569203974449:web:298a8db29861c0b2119010"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
